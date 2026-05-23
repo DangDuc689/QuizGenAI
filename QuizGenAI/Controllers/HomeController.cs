860e921c35c335c3071d994ca1ef5fb8e37c84d7
DangDuc689 <dangngocduc0913@gmail.com>
2026-05-24 00:17:07 +0700

    cau truc project

---
+++ QuizGenAI/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using QuizGenAI.Models;
+using System.Diagnostics;
+
+namespace QuizGenAI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
