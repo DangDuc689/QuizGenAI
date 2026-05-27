0bdffd788390cdeb2849958667167dba0b0f1656
Lê An <letanbinhan1509@gmail.com>
2026-05-28 01:12:24 +0700

    Add documents UI module

---
+++ QuizGenAI/Controllers/DocumentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizGenAI.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["ActivePage"] = "Documents";
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            ViewData["ActivePage"] = "Documents";
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewData["ActivePage"] = "Documents";
+            ViewBag.DocumentId = id;
+            return View();
+        }
+    }
+}
